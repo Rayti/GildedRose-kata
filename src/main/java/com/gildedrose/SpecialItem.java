30f9a96ed1bedd82a5dc091635f2bccd3bccf370
Karlowicz <milosz.karlowicz@capgemini.com>
2021-04-03 15:59:33 +0200

    Add tests for conjured items

--- src/main/java/com/gildedrose/SpecialItem.java
+++ src/main/java/com/gildedrose/SpecialItem.java
@@ -15,11 +15,11 @@ public enum SpecialItem {
         if(item.sellIn <= 0){
             item.quality = 0;
         }else if (item.sellIn <= 5){
-            item.quality += 3;
+            item.quality += 3 * multiplier;
         }else if (item.sellIn <= 10){
-            item.quality += 2;
+            item.quality += 2 * multiplier;
         }else{
-            item.quality++;
+            item.quality += 1 * multiplier;
         }
         checkQualityBoundaries(item);
         item.sellIn--;
--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -12,8 +12,8 @@ class GildedRoseTest {
 
     private GildedRose gildedRose;
 
-    //naming strategy: UnitOfWork_StateUnderTest_ExpectedBehavior
 
+    //naming strategy: UnitOfWork_StateUnderTest_ExpectedBehavior
     @Test
     void updateQuality_SulfurasQuality_ConstantQuality(){
         String sulfurasName = "Sulfuras, Hand of Ragnaros";
@@ -21,6 +21,20 @@ class GildedRoseTest {
         Item [] items = new Item[] {new Item(sulfurasName, -5, quality),
             new Item(sulfurasName, 0, quality),
             new Item(sulfurasName, 10, quality)};
+        gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
+        assertEquals(quality, gildedRose.items[0].quality);
+        assertEquals(quality, gildedRose.items[1].quality);
+        assertEquals(quality, gildedRose.items[2].quality);
+    }
+
+    @Test
+    void updateQuality_ConjuredSulfurasQuality_ConstantQuality(){
+        String sulfurasName = "Conjured Sulfuras, Hand of Ragnaros";
+        int quality = 80;
+        Item [] items = new Item[] {new Item(sulfurasName, -5, quality),
+                new Item(sulfurasName, 0, quality),
+                new Item(sulfurasName, 10, quality)};
         gildedRose = new GildedRose(items);
         gildedRose.updateQuality();
         assertEquals(quality, gildedRose.items[0].quality);
@@ -45,9 +59,12 @@ class GildedRoseTest {
     @Test
     void updateQuality_DefaultItemQualityLowerBoundary_DoesNotExceedBoundaryLimit(){
         Item [] items = new Item[] {new Item("common item", 10, 10),
-            new Item("common item 2", 10, 0), new Item("common item 3", 0,  10),
-            new Item("common item 4", 0, 0), new Item("common item 5", -5, 10),
-            new Item("common item 5", -5, 0)};
+                new Item("common item 2", 10, 0), new Item("common item 3", 0,  10),
+                new Item("common item 4", 0, 0), new Item("common item 5", -5, 10),
+                new Item("common item 5", -5, 0), new Item("Conjured common item", 10, 10),
+                new Item("Conjured common item 2", 10, 0), new Item("Conjured common item 3", 0,  10),
+                new Item("Conjured common item 4", 0, 0), new Item("Conjured common item 5", -5, 10),
+                new Item("Conjured common item 5", -5, 0)};
         gildedRose = new GildedRose(items);
         gildedRose.updateQuality();
         Arrays.stream(gildedRose.items)
@@ -59,7 +76,10 @@ class GildedRoseTest {
         Item [] items = new Item[] {new Item("common item", 10, 51),
                 new Item("common item 2", 10, 50), new Item("common item 3", 0,  10),
                 new Item("common item 4", 0, 49), new Item("common item 5", -5, 10),
-                new Item("common item 5", -5, 51)};
+                new Item("common item 5", -5, 51), new Item("Conjured common item", 10, 51),
+                new Item("Conjured common item 2", 10, 50), new Item("Conjured common item 3", 0,  10),
+                new Item("Conjured common item 4", 0, 49), new Item("Conjured common item 5", -5, 10),
+                new Item("Conjured common item 5", -5, 51) };
         gildedRose = new GildedRose(items);
         gildedRose.updateQuality();
         Arrays.stream(gildedRose.items)
@@ -75,6 +95,17 @@ class GildedRoseTest {
         assertEquals(gildedRose.items[0].quality, 9);
         assertEquals(gildedRose.items[1].quality, 8);
         assertEquals(gildedRose.items[2].quality, 8);
+    }
+
+    @Test
+    void updateQuality_ConjuredDefaultItemQualityChange_QualityDecreased(){
+        Item [] items = new Item[] {new Item("Conjured common item", 10, 10),
+                new Item("Conjured common item 2", 0, 10), new Item("Conjured common item 3", -5,  10)};
+        gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
+        assertEquals(gildedRose.items[0].quality, 8);
+        assertEquals(gildedRose.items[1].quality, 6);
+        assertEquals(gildedRose.items[2].quality, 6);
     }
 
     @Test
@@ -102,6 +133,18 @@ class GildedRoseTest {
     }
 
     @Test
+    void updateQuality_ConjuredAgedBrieQuality_QualityIncreased(){
+        String name = "Conjured Aged Brie";
+        Item[] items = new Item [] {new Item(name, 10, 10),
+                new Item(name, 0, 10), new Item(name, -5, 10)};
+        gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
+        assertEquals(gildedRose.items[0].quality, 12);
+        assertEquals(gildedRose.items[1].quality, 14);
+        assertEquals(gildedRose.items[2].quality, 14);
+    }
+
+    @Test
     void updateQuality_BackstagePassesSellIn_SellInDecreased(){
         String name = "Backstage passes";
         Item[] items = new Item[] {new Item(name, 10, 10), new Item(name, 0, 10),
@@ -126,6 +169,23 @@ class GildedRoseTest {
         assertEquals(gildedRose.items[2].quality, 12);
         assertEquals(gildedRose.items[3].quality, 13);
         assertEquals(gildedRose.items[4].quality, 13);
+        assertEquals(gildedRose.items[5].quality, 0);
+        assertEquals(gildedRose.items[6].quality, 0);
+    }
+
+    @Test
+    void updateQuality_ConjuredBackstagePassesQuality_QualityDependingOnSellIn(){
+        String name = "Conjured Backstage passes to a TAFKAL80ETC concert";
+        Item[] items = new Item[] {new Item(name, 20, 10), new Item(name, 10, 10),
+                new Item(name, 7, 10), new Item(name, 5, 10), new Item(name, 2, 10),
+                new Item(name, 0, 10), new Item(name, -5, 10)};
+        gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
+        assertEquals(gildedRose.items[0].quality, 12);
+        assertEquals(gildedRose.items[1].quality, 14);
+        assertEquals(gildedRose.items[2].quality, 14);
+        assertEquals(gildedRose.items[3].quality, 16);
+        assertEquals(gildedRose.items[4].quality, 16);
         assertEquals(gildedRose.items[5].quality, 0);
         assertEquals(gildedRose.items[6].quality, 0);
     }
